d605d76601312e90f1474336b9a1e04e531e8437
Denis Kravtsov <worker.hot@gmail.com>
2015-08-30 23:05:50 +0300

    infoblocks

--- views/site/index.php
+++ views/site/index.php
@@ -2,5 +2,19 @@
 $this->title = 'Альфаро';
 ?>
 <div class="site-index">
-    <img src="/img/banner.jpg" alt=""/>
+    <div class="index-banner">
+        <img src="/img/banner.jpg" alt=""/>
+    </div>
+    <div class="index-content">
+        <div class="index-infoblocks">
+            <div class="infoblocks">
+                <div class="infoblock-title">Нам доверяют</div>
+                <div class="infoblock-content"></div>
+            </div>
+            <div class="infoblocks">
+                <div class="infoblock-title">Вопрос-ответ</div>
+                <div class="infoblock-content"></div>
+            </div>
+        </div>
+    </div>
 </div>
