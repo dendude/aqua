d228923c9475dac2c4f02dc882262e306342b959
Denis Kravtsov <worker.hot@gmail.com>
2015-11-26 01:11:24 +0300

    links

--- modules/admin/views/pages/add.php
+++ modules/admin/views/pages/add.php
@@ -42,6 +42,13 @@ $menu_filter = $root_menu ? \yii\helpers\ArrayHelper::map($root_menu, 'id', 'men
         <div class="well">
             <?= $form->field($model, 'title') ?>
             <?= \app\helpers\MHtml::aliasField($model, 'alias', 'alias') ?>
+            <? if ($model->id): ?>
+                <div class="form-group" style="margin: -10px 0 15px 0;">
+                    <div class="col-xs-offset-4 col-xs-8">
+                        <a href="http://akvarium-moskva.ru/<?= $model->alias ?>.php" target="_blank">Ссылка на сайт-источник &raquo;</a>
+                    </div>
+                </div>
+            <? endif; ?>
             <?= \app\helpers\MHtml::aliasField($model, 'alias_new', 'alias_new', false, 'Если заполнено - сюда будет происходить 301 редирект') ?>
             <div class="form-group">
                 <div class="col-xs-offset-4 col-xs-8 text-muted small">
