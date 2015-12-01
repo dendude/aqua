9b86a8cdef0a94711c6040dbf3a177c986b6417e
Denis Kravtsov <worker.hot@gmail.com>
2015-12-02 00:03:48 +0300

    style

--- modules/admin/views/templates/blue-block-horizontal.php
+++ modules/admin/views/templates/blue-block-horizontal.php
@@ -1,18 +1,20 @@
 <div class="v-block v-horizontal">
-    <table class="table-auto table-padding5">
-        <tbody>
-            <tr>
-                <td style="vertical-align: top;">место для фото</td>
-                <td style="vertical-align: top;">место для фото</td>
-                <td style="vertical-align: top;">место для фото</td>
-                <td style="vertical-align: top;">место для фото</td>
-            </tr>
-            <tr>
-                <td style="vertical-align: top;"><small class="m-b">текст</small></td>
-                <td style="vertical-align: top;"><small class="m-b">текст</small></td>
-                <td style="vertical-align: top;"><small class="m-b">текст</small></td>
-                <td style="vertical-align: top;"><small class="m-b">текст</small></td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="clearfix">
+        <table class="table-padding5">
+            <tbody>
+                <tr>
+                    <td style="vertical-align: top;">место для фото</td>
+                    <td style="vertical-align: top;">место для фото</td>
+                    <td style="vertical-align: top;">место для фото</td>
+                    <td style="vertical-align: top;">место для фото</td>
+                </tr>
+                <tr>
+                    <td style="vertical-align: top;"><small class="m-b">текст</small></td>
+                    <td style="vertical-align: top;"><small class="m-b">текст</small></td>
+                    <td style="vertical-align: top;"><small class="m-b">текст</small></td>
+                    <td style="vertical-align: top;"><small class="m-b">текст</small></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
