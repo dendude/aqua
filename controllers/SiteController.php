9b3aa3a4131e4840881a77ed1feb248b642a5351
Denis Kravtsov <worker.hot@gmail.com>
2015-12-18 23:57:20 +0300

    bg and error

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -374,6 +374,14 @@ class SiteController extends Controller
         ]);
     }
 
+    public function actionError()
+    {
+        $exception = Yii::$app->errorHandler->exception;
+        if ($exception !== null) {
+            return $this->render('error', ['exception' => $exception]);
+        }
+    }
+
     public function actionSitemap() {
 
         $sitemap = new DSitemap();
